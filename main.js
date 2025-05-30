241abfc0a68c0d601a0fc5719f460fa5f8e5d6fd
Suco de Laranja <143010678+SucoDeLaranja25@users.noreply.github.com>
2025-05-30 12:05:55 -0300

    s

--- main.js
+++ main.js
@@ -22,6 +22,25 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
 }
 
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+campoSenha.value = letrasMaiusculas;
+
+
+function geraSenha(){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    console.log(letrasMaiusculas[numeroAleatorio]);
+}
+
+
+
+
+
+
 <input name="senha" type="text" id="campo-senha" readonly></input>
 
 console.log(botoes)
