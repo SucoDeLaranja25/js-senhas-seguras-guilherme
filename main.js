96c85ad17f82efb12391cd3564df08b898debf91
Suco de Laranja <143010678+SucoDeLaranja25@users.noreply.github.com>
2025-05-30 11:44:29 -0300

    s

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelector('parametro-senha_botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1
+    numeroSenha.textContent = tamanhoSenha
+}
+console.log(botoes)
