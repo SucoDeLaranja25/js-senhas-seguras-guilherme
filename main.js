2ea2c683b35a6ac958c49edb2f691ddc05c1ceaf
Suco de Laranja <143010678+SucoDeLaranja25@users.noreply.github.com>
2025-06-06 12:07:40 -0300

    a

--- main.js
+++ main.js
@@ -30,44 +30,52 @@ function aumentaTamanho() {
     geraSenha();
 }
 
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
 geraSenha();
 
 function geraSenha() {
     let alfabeto = '';
-    if (checkbox[0].checked){
+    if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
     }
-    if (checkbox[1].checked){
+    if (checkbox[1].checked) {
         alfabeto = alfabeto + letrasMinusculas;
     }
-    if (checkbox[2].checked){
+    if (checkbox[2].checked) {
         alfabeto = alfabeto + numeros;
     }
-    if (checkbox[3].checked){
+    if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
     classificaSenha(alfabeto.length);
+
 }
 
-function classificaSenha(tamanhoAlfabeto){
+function classificaSenha(tamanhoAlfabeto) {
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     console.log(entropia);
-    forcaSenha.classList.remove('fraca','media','forte');
-    if (entropia > 57){
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57) {
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 35 && tamanhoSenha < 57 ) {
+    } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 35){
-        forcaSenha.classList.add('fraca')
+    } else if (entropia <= 35) {
+        forcaSenha.classList.add('fraca');
     }
-    const valorEntropia = document.querySelector(".entropia");
-    valorEntropia.textContent = "Um Computador pode levar até:" + Math.floor(2**entropia/(1006*60*60*24)) + "dias para descobrir a senha.";
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
 }
+
+
+
+
